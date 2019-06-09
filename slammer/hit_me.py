7f411447be2e44b96bb4ef367749e7738a5ef8f5
jackkeck <2229632+jackkeck@users.noreply.github.com>
2019-06-09 15:46:10 -0400

    added docker-compose for swarm

--- slammer/hit_me.py
+++ slammer/hit_me.py
@@ -2,33 +2,33 @@ import requests
 import json
 
 def loginAsUser(username, password):
-    url = 'http://127.0.0.1:5000/api/v1/login'
+    url = 'http://localhost:5000/api/v1/login'
     headers = {'Content-Type': "application/json"}
     data = {"username": username, "password": password}
     response = requests.post(url, headers=headers, json=data)
     print ("loginAsUser("+str(username)+", password) returns: "+ str(response.status_code))
 
 def makeTransfer(username, toUser, amount):
-    url = 'http://127.0.0.1:5000/api/v1/transfer'
+    url = 'http://localhost:5000/api/v1/transfer'
     headers = {'Content-Type': "application/json"}
     data = {"username": username, "toUser": toUser, "amount": amount}
     response = requests.post(url, headers=headers, json=data)
     print ("makeTransfer("+str(username)+", "+str(toUser)+", "+str(amount)+") returns: "+str(response.text))
 
 def accountSummary(username):
-    url = 'http://127.0.0.1:5000/api/v1/account'
+    url = 'http://localhost:5000/api/v1/account'
     headers = {'Content-Type': "application/json"}
     data = {"username": username}
     response = requests.post(url, headers=headers, json=data)
     print ("accountSummary("+str(username)+") returns: "+str(response.text))
 
 def help():
-    url = 'http://127.0.0.1:5000/api/v1/help'
+    url = 'http://localhost:5000/api/v1/help'
     response = requests.get(url)
     print ("help() returns: "+ str(response.status_code))
 
 def logoutAsUser(username):
-    url = 'http://127.0.0.1:5000/api/v1/logout'
+    url = 'http://localhost:5000/api/v1/logout'
     headers = {'Content-Type': "application/json"}
     data = {"username": username}
     response = requests.post(url, headers=headers, json=data)
